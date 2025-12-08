65c681dbf36219e0e015d539af1036c40921b4d5
Alessandro Antunes da silva <72531460+alessandro339@users.noreply.github.com>
2025-12-07 21:36:09 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Rotas usuário
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Booking from './pages/Booking';
+
+// Rotas admin
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
-import AdminRoute from './pages/AdminRoute';
+import AdminRoute from './pages/AdminRoute'; // componente de proteção de rota admin
 
-function App(){
+function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/booking" element={<Booking/>} />
-          <Route path="/admin" element={
-            <AdminRoute>
-              <AdminDashboard />
-            </AdminRoute>
-          } />
-          <Route path="/admin" element={<AdminDashboard/>} />
+          {/* Rotas usuário */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/booking" element={<Booking />} />
+
+          {/* Rotas admin */}
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route
+            path="/admin"
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
+
 export default App;
